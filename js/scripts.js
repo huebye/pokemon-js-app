014e77ab6460cfd2fc1880ee0eff10d715d0771a
huebye <thomashuebner94@gmx.de>
2021-03-18 20:29:38 +0100

    added PokemonList
    included name, height, type, attack and defense of 9 different pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-alert('Hello world');
-let favouriteFood='Pizza';
-document.write(favouriteFood);
+let PokemonList = [{name: "Charizard", height: 1.7, type: ["fire", "flying"], attack: 84, defense: 78},
+                  {name: "Pikachu", height: 0.4, type: ["electric", "fairy"], attack: 55, defense: 40},
+                  {name: "Ninetales", height: 1.1, type: ["fire", "field"], attack: 76, defense: 75},
+                  {name: "Wobbuffet", height: 1.3, type: ["psychic", "telepathy"], attack: 33, defense: 58},
+                  {name: "Blastoise", height: 1.6, type: ["water", "monster"], attack: 83, defense: 100},
+                  {name: "Clefable", height: 1.3, type: ["fairy", "poison"], attack: 70, defense: 73},
+                  {name: "Nidoking", height: 1.4, type: ["ground", "poison"], attack: 102, defense: 77},
+                  {name: "Ursaring", height: 1.8, type: ["normal", "field"], attack: 130, defense: 75},
+                  {name: "Genesect", height: 1.5, type: ["steel", "bug"], attack: 120, defense: 95}]
