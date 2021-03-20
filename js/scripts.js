93701623620434400cb541c227c027d0ce02fbc9
huebye <thomashuebner94@gmx.de>
2021-03-20 13:55:27 +0100

    update PokemonList
    displayed list with document.write
    styled app in css
    structured html

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,12 @@ let PokemonList = [{name: "Charizard", height: 1.7, type: ["fire", "flying"], at
                   {name: "Clefable", height: 1.3, type: ["fairy", "poison"], attack: 70, defense: 73},
                   {name: "Nidoking", height: 1.4, type: ["ground", "poison"], attack: 102, defense: 77},
                   {name: "Ursaring", height: 1.8, type: ["normal", "field"], attack: 130, defense: 75},
-                  {name: "Genesect", height: 1.5, type: ["steel", "bug"], attack: 120, defense: 95}]
+                  {name: "Genesect", height: 1.5, type: ["steel", "bug"], attack: 120, defense: 95}];
+
+for (let i=0; i < PokemonList.length; i++){
+  if (PokemonList[i].height <1.5) {
+    document.write(PokemonList[i].name + " (height: " + (PokemonList[i].height) + ") <br>");
+  } else if (PokemonList[i].height >1.4) {
+    document.write(PokemonList[i].name + " (height: " + (PokemonList[i].height) + ") - Wow, that's big!!!<br>");
+  }
+}
