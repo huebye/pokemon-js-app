b57d58ab39fa35d84a35b1f640da56a6ac2789fe
huebye <thomashuebner94@gmx.de>
2021-03-26 12:45:18 +0100

    new condition for add function

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,9 @@ let pokemonRepository = (function() {
                     ];
 
     function add(item) {
-      if (typeof item === 'object'){
+      if (typeof item === 'object' && item === Object.keys(pokemonList)){
       pokemonList.push(item);
-    }
+     }
     }
 
     function getAll() {
@@ -33,5 +33,6 @@ function myLoopFunction(pokemon) {
     document.write('<p>' + pokemon.name + " (height: " + (pokemon.height) + ") - Wow, that's big!!!" + '</p>');
   }
 }
+
 
 pokemonRepository.getAll();
